8a80c529f8764394168a84f388d282970909c9a3
Ehsanera <ehsan-rafee@hotmail.com>
2021-11-04 13:14:59 +0330

    find the best performance function

--- src/main/kotlin/com/Main.kt
+++ src/main/kotlin/com/Main.kt
@@ -1,6 +1,28 @@
 package com
 
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
+
 fun main() {
+    println("check performance")
+    val time1 = measureNanoTime {
+        Solutions().twoSumBasic(intArrayOf(1, 5, 8, 6, 9, 2, 3, 4, 7), 9)
+    }
+    val time2 = measureNanoTime {
+        Solutions().twoSumBruteForce(intArrayOf(1, 5, 8, 6, 9, 2, 3, 4, 7), 9)
+    }
+    val time3 = measureNanoTime {
+        Solutions().twoSumHashTable(intArrayOf(1, 5, 8, 6, 9, 2, 3, 4, 7), 9)
+    }
+    val time4 = measureNanoTime {
+        Solutions().onePassHashTable(intArrayOf(1, 5, 8, 6, 9, 2, 3, 4, 7), 9)
+    }
+    println(time1)
+    println(time2)
+    println(time3)
+    println(time4)
+
+    println("\nfunctions run")
     println(Solutions().twoSumBasic(intArrayOf(1, 5, 8, 6, 9, 2, 3, 4, 7), 9))
     println(Solutions().twoSumBruteForce(intArrayOf(1, 5, 8, 6, 9, 2, 3, 4, 7), 9))
     println(Solutions().twoSumHashTable(intArrayOf(1, 5, 8, 6, 9, 2, 3, 4, 7), 9))
